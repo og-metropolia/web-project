f7d1d69a6b5232666cf8d64601dc6d0f5f3e247d
4MBL <74970679+4MBL@users.noreply.github.com>
2022-12-01 18:27:59 +0200

    create global navbar

---
+++ conponents/navbar.js
@@ -0,0 +1,13 @@
+let logoUrl = new URL('../../img/logo.png', document.baseURI).href;
+
+document.write(`
+<nav class="nav">
+  <ul>
+    <li><img id="logo" src="${logoUrl}" alt="OG Logistic Services logo" /></li>
+    <li><a href="/">ETUSIVU</a></li>
+    <li><a href="pages/yhteystiedot">YHTEYSTIEDOT</a></li>
+    <li><a href="pages/tietoa-meista">MEISTÄ</a></li>
+    <li><a href="pages/paastolaskuri/">PÄÄSTÖLASKURI</a></li>
+  </ul>
+</nav>
+`);
