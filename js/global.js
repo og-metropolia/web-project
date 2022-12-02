9d4e022923dac350a826383033a232c63c31617f
4MBL <74970679+4MBL@users.noreply.github.com>
2022-12-02 11:37:47 +0200

    move navbar to global.js

---
+++ js/global.js
@@ -0,0 +1,13 @@
+let navbarLogoUrl = new URL('../../img/logo.png', document.baseURI).href;
+
+document.write(`
+<nav class="nav">
+  <ul>
+    <li><img id="logo" src="${navbarLogoUrl}" alt="OG Logistic Services logo" /></li>
+    <li><a href="/">ETUSIVU</a></li>
+    <li><a href="pages/yhteystiedot">YHTEYSTIEDOT</a></li>
+    <li><a href="pages/tietoa-meista">MEISTÄ</a></li>
+    <li><a href="pages/paastolaskuri/">PÄÄSTÖLASKURI</a></li>
+  </ul>
+</nav>
+`);
