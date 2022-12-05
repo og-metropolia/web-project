4c09df4b0ddc64758235c1b4ec2af847f78b8ad2
4MBL <74970679+4MBL@users.noreply.github.com>
2022-12-05 11:48:06 +0200

    fix homepage link in navbar

--- js/global.js
+++ js/global.js
@@ -4,7 +4,7 @@ document.write(`
 <nav class="nav">
   <ul>
     <li><img id="logo" src="${navbarLogoUrl}" alt="OG Logistic Services logo" /></li>
-    <li><a href="/">ETUSIVU</a></li>
+    <li><a href="./">ETUSIVU</a></li>
     <li><a href="pages/yhteystiedot">YHTEYSTIEDOT</a></li>
     <li><a href="pages/tietoa-meista">MEISTÄ</a></li>
     <li><a href="pages/paastolaskuri/">PÄÄSTÖLASKURI</a></li>
