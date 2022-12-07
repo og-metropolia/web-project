9a9202d51122bfa9115de00ccc51502f436858d7
gashi <gashiad@outlook.com>
2022-12-07 12:57:27 +0200

    Created global footer and edited in global.css

--- js/footer.js
+++ js/footer.js
@@ -1,21 +1,31 @@
 document.write(`
-<div className="footer">
-    <div className="col-1">
-        <h3>USEFUL LINKS</h3>
-        <a href="#">About us</a>
-        <a href="#">Ota yhteytta</a>
+<script src="https://kit.fontawesome.com/d526bb85bd.js" crossorigin="anonymous"></script>
+<div class="footer">
+    <div class="col-1">
+        <h3>OG Logistic Services</h3>
+        <p>Leppävaarankatu 11 C</p>
+        <p>02600 Espoo</p>
+        <p>Y-tunnus 2330662-6 </p>
     </div>
-    <div className="col-2">
-        <h3>TILAA UUTISKIRJE</h3>
+    <div class="col-2">
+        <h3>Tilaa uutiskirje</h3>
         <form>
-            <input type="text" placeholder="Syötä sähköpostiosoitteesi">
-                <button type="submit">TILAA</button>
+            <input type="email" placeholder="Syötä sähköpostiosoitteesi" required>
+            <br>
+            <button type="submit">TILAA</button>
         </form>
     </div>
-    <div className="col-3">
-        <h3>YHTEYSTIEDOT</h3>
-        <p>Leppävaarankatu 11 C <br></p>
-        <p>02600 Espoo</p>
+    <div class="col-3">
+        <h3>Tilauskeskus</h3>
+        <p>09 720 65200</p>
+        <p>ma-pe 7.30-19.00</p>
+        <a id="footermail" href="mailto:info@oglogistic.fi">info@oglogistic.fi</a>
+        <div class="social-icons">
+        <i class="fab fa-facebook"></i>
+        <i class="fab fa-instagram"></i>
+        <i class="fab fa-twitter"></i>
+        </div>
+ 
     </div>
 </div>
 `);
