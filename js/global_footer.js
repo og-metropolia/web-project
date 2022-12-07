dc9e58409dcfd343130f6df541d93be7afe53ac4
gashi <gashiad@outlook.com>
2022-12-07 11:45:26 +0200

    Added global_footer.js

---
+++ js/global_footer.js
@@ -0,0 +1,22 @@
+document.write(`
+<div className="footer">
+    <div className="col-1">
+        <h3>USEFUL LINKS</h3>
+        <a href="#">About us</a>
+        <a href="#">Ota yhteytta</a>
+    </div>
+    <div className="col-2">
+        <h3>TILAA UUTISKIRJE</h3>
+        <form>
+            <input type="text" placeholder="Syötä sähköpostiosoitteesi">
+                <button type="submit">TILAA</button>
+        </form>
+    </div>
+    <div className="col-3">
+        <h3>YHTEYSTIEDOT</h3>
+        <p>Leppävaarankatu 11 C <br></p>
+        <p>02600 Espoo</p>
+    </div>
+</div>
+`);
+
