bca545745b14e6e9c95bd4baa8c969c12ba00021
4MBL <74970679+4MBL@users.noreply.github.com>
2022-12-08 10:54:13 +0200

    update favicon & navbar link paths

--- js/global.js
+++ js/global.js
@@ -4,10 +4,10 @@ document.write(`
 <nav class="nav">
   <ul>
     <li><img id="logo" src="${navbarLogoUrl}" alt="OG Logistic Services logo" /></li>
-    <li><a href="../../pages/etusivu/">ETUSIVU</a></li>
-    <li><a href="../../pages/yhteystiedot/">YHTEYSTIEDOT</a></li>
-    <li><a href="../../pages/tietoa-meista/">MEISTÄ</a></li>
-    <li><a href="../../pages/paastolaskuri/">PÄÄSTÖLASKURI</a></li>
+    <li><a href="../../sivut/etusivu/">ETUSIVU</a></li>
+    <li><a href="../../sivut/yhteystiedot/">YHTEYSTIEDOT</a></li>
+    <li><a href="../../sivut/tietoa-meista/">MEISTÄ</a></li>
+    <li><a href="../../sivut/paastolaskuri/">PÄÄSTÖLASKURI</a></li>
   </ul>
 </nav>
 `);
