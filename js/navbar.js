d0b0fb0d608e806034920c574c1a42898c473c1a
4MBL <74970679+4MBL@users.noreply.github.com>
2022-12-10 21:26:28 +0200

    make navbar logo clickable

--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,7 @@ let navbarLogoUrl = new URL('../../img/logo.png', document.baseURI).href;
 document.write(`
 <nav class="nav">
   <ul>
-    <li><img id="logo" src="${navbarLogoUrl}" alt="OG Logistic Services logo" /></li>
+    <li><a href="../../"><img id="logo" src="${navbarLogoUrl}" alt="OG Logistic Services logo" /></a></li>
     <li><a href="../../sivut/etusivu/">ETUSIVU</a></li>
     <li><a href="../../sivut/yhteystiedot/">YHTEYSTIEDOT</a></li>
     <li><a href="../../sivut/tietoa-meista/">MEISTÄ</a></li>
