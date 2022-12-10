7b3b311867023b7e43c452e5364924e510cc6df6
4MBL <74970679+4MBL@users.noreply.github.com>
2022-12-10 22:09:33 +0200

    change navbar "etusivu" link to point to site root

--- js/navbar.js
+++ js/navbar.js
@@ -4,7 +4,7 @@ document.write(`
 <nav class="nav">
   <ul>
     <li><a href="../../"><img id="logo" src="${navbarLogoUrl}" alt="OG Logistic Services logo" /></a></li>
-    <li><a href="../../sivut/etusivu/">ETUSIVU</a></li>
+    <li><a href="../../">ETUSIVU</a></li>
     <li><a href="../../sivut/yhteystiedot/">YHTEYSTIEDOT</a></li>
     <li><a href="../../sivut/tietoa-meista/">MEISTÄ</a></li>
     <li><a href="../../sivut/paastolaskuri/">PÄÄSTÖLASKURI</a></li>
