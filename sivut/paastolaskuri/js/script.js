59a10f356f40f4700ce18c0771a120f8d7351be9
4MBL <74970679+4MBL@users.noreply.github.com>
2022-12-13 21:25:45 +0200

    comment code

--- sivut/paastolaskuri/js/script.js
+++ sivut/paastolaskuri/js/script.js
@@ -1,3 +1,7 @@
+/**
+ * @author Leo H / @4MBL
+ */
+
 const API_KEY = 'AIzaSyCLeW9dofrYcuHtUEYNpC2xydSTB9ud3zM';
 
 const btn = document.querySelector('#search-button');
@@ -14,11 +18,8 @@ btn.addEventListener('click', async (event) => {
   let sourceAddressPlus = toPlusNotation(sourceSearchInput);
   let destinationAddressPlus = toPlusNotation(destinationSearchInput);
 
-  let sourceAddressCoordsJson = await toCoords(sourceAddressPlus);
-  let destinationAddressCoordsJson = await toCoords(destinationAddressPlus);
-
-  let destinationAddressCoords = getCoords(destinationAddressCoordsJson);
-  let sourceAddressCoords = getCoords(sourceAddressCoordsJson);
+  let destinationAddressCoords = await toCoords(sourceAddressPlus);
+  let sourceAddressCoords = await toCoords(destinationAddressPlus);
 
   try {
     let data = await getData(
@@ -46,6 +47,15 @@ btn.addEventListener('click', async (event) => {
   }
 });
 
+/**
+ * Fetches journey data from Google Maps API.
+ *
+ * @param {int} originLatitude
+ * @param {int} originLongitude
+ * @param {int} destinationLatitude
+ * @param {int} destinationLongitude
+ * @return {json} response data
+ */
 const getData = async (oLat, oLng, dLat, dLng) => {
   const MAPS_URL = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${oLat}%2C${oLng}&destinations=${dLat}%2C${dLng}&key=${API_KEY}`;
   const ENCODED_URL = encodeURIComponent(MAPS_URL);
@@ -55,38 +65,72 @@ const getData = async (oLat, oLng, dLat, dLng) => {
   return json;
 };
 
+/**
+ * Gets journey distance from the data.
+ *
+ * @param {json} data from a fetch to the Google Maps API
+ * @return {string} distance in meters
+ */
 function getDistance(data) {
   return data.rows[0].elements[0].distance.value; // returns in meters
 }
 
+/**
+ * Gets journey duration from the data.
+ *
+ * @param {json} data from a fetch to the Google Maps API
+ * @return {string} duration in seconds
+ */
 function getDuration(data) {
-  return data.rows[0].elements[0].duration.value; // returns in seconds
+  return data.rows[0].elements[0].duration.value;
 }
 
+/**
+ * Changes spaces to plusses.
+ *
+ * @param {string} text
+ * @return {string} plusText
+ */
 function toPlusNotation(text) {
   return text.replaceAll(' ', '+');
 }
 
+/**
+ * Calculates emissions based on distance and weight.
+ *
+ * @param {int} distance in meters
+ * @param {int} weight in kilograms
+ * @return {int} co2 emissions
+ */
 function calculateEmissions(distanceMeters, weightKilograms) {
   const CO2_PER_METER = 6.5 * 10 ** -8;
   return distanceMeters * CO2_PER_METER * weightKilograms;
 }
 
+/**
+ * Converts an address to coordinates using Google Maps API.
+ *
+ * @param {string} address
+ * @return {[latitude, longitude]} coordinates
+ */
 const toCoords = async (address) => {
   const MAPS_URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${API_KEY}`;
   const ENCODED_URL = encodeURIComponent(MAPS_URL);
   const QUERY_URL = toCorsSave(ENCODED_URL);
   const response = await fetch(QUERY_URL);
   const json = await response.json();
-  return json;
-};
-
-function getCoords(json) {
   let lat = json.results[0].geometry.location.lat;
   let lng = json.results[0].geometry.location.lng;
   return [lat, lng];
-}
+};
 
+/**
+ * Validates coordinates.
+ *
+ * @param {int} latitude
+ * @param {int} longitude
+ * @return {boolean} whether inputs are coordinates
+ */
 function validateCoords(lat, lng) {
   const regexLat = /^(-?[1-8]?\d(?:\.\d{1,18})?|90(?:\.0{1,18})?)$/;
   const regexLng = /^(-?(?:1[0-7]|[1-9])?\d(?:\.\d{1,18})?|180(?:\.0{1,18})?)$/;
@@ -95,18 +139,43 @@ function validateCoords(lat, lng) {
   return validLat && validLng;
 }
 
+/**
+ * Simple meter to kilometer converter.
+ *
+ * @param {int} meters
+ * @return {int} kilometers
+ */
 function toKilometers(meters) {
   return meters / 1000;
 }
 
+/**
+ * Simple second to hour converter.
+ *
+ * @param {int} seconds
+ * @return {int} hours
+ */
 function toHours(seconds) {
   return seconds / 3600;
 }
 
+/**
+ * Changes decimal separator from a dot (.) to a comma (,).
+ *
+ * @param {int} number
+ * @return {string} number with comma as decimal separator
+ */
 function toCommaSep(number) {
   return number.toString().replace('.', ',');
 }
 
+/**
+ * Shows the journey info to the user.
+ *
+ * @param {int} distance in meters
+ * @param {int} duration in seconds
+ * @param {int} emissions in kg co2
+ */
 function showValues(distance, duration, emissions) {
   let outputHeading = document.querySelector('#output-heading');
   outputHeading.style.visibility = 'visible';
@@ -128,6 +197,14 @@ function showValues(distance, duration, emissions) {
   durationOutputElem.style.visibility = 'visible';
 }
 
+/**
+ * Shows journey map to the user.
+ *
+ * @param {int} originLatitude
+ * @param {int} originLongitude
+ * @param {int} destinationLatitude
+ * @param {int} destinationLongitude
+ */
 function drawMap(originLat, originLng, destLat, destLng) {
   const mapCanvasElem = document.querySelector('#map-canvas');
 
@@ -187,6 +264,12 @@ function calculateAndDisplayRoute(
   );
 }
 
+/**
+ * Makes fetch URL CORS safe by using a proxy.
+ *
+ * @param {string} url
+ * @return {string} cors safe url
+ */
 function toCorsSave(url) {
   // const PROXY_URL = 'https://api.allorigins.win/get?url=';
   // const PROXY_URL = 'https://cors-proxy.htmldriven.com/?url=';
